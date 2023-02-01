a8be52604900860a76dc19031b9ac8bc09d7b728
thideve <107429305+thideve@users.noreply.github.com>
2023-02-01 13:54:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet foi inicializado');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX);
+    }
+}
+function draw() {
+    background('#6C91C2');
+    
+      document.getElementById("font_size").innerHTML = "Tamanho da fonte será = " + difference + "px";
+    textSize(difference);
+    fill('#FFE787');
+    text('Thiago', 50, 400);
+    }
+
